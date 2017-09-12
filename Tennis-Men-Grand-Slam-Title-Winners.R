40333a01dab7c6799e6b715ece77439a01ffe405
Chetan Singh Lodhi <noddy.chetan@gmail.com>
2017-09-13 00:50:27 +0530

    Update Tennis-Men-Grand-Slam-Title-Winners.R

--- Tennis-Men-Grand-Slam-Title-Winners.R
+++ Tennis-Men-Grand-Slam-Title-Winners.R
@@ -1,26 +1,36 @@
 setwd("D:/R/Projects/Tennis")
 
+# Loading R libraries
 library_toload <- c("dplyr", "knitr", "ggplot2", "gplots", "RColorBrewer", "timelineS", "circlize", "fmsb")
 invisible(lapply(library_toload, function(x) {suppressPackageStartupMessages(library(x, character.only=TRUE))}))
 
+# Importing the Tennis Grand Slam Winners dataset
 Slam_Winners <- read.table("Tennis-Men-Grand-Slam-Title-Winners.txt", sep="\t", stringsAsFactors = FALSE, header = TRUE)
 
+# Fix to the tournaments data column to have same naming for the Australian Open tournament as it held twice in 1977
 Slam_Winners[grep("Australian Open", Slam_Winners$TOURNAMENT), "TOURNAMENT"] = "Australian Open"
 
+# Grouping by winner and summarising the number of wins
 Slam_Top_Chart = Slam_Winners %>% group_by(WINNER) %>% summarise(NUM_WINS=n()) %>% arrange(desc(NUM_WINS))
-
 Slam_Top_Chart$WINNER <- factor(Slam_Top_Chart$WINNER, levels = Slam_Top_Chart$WINNER[order(Slam_Top_Chart$NUM_WINS)])
+
+# Champions with Grand Slam Singles Titles (4 or more)
 top_winners_gt4 = Slam_Top_Chart %>% filter(NUM_WINS >= 4)
+
 the_colours = c("#FF4000FF", "#FF8000FF", "#FFFF00FF", "#80FF00FF",
                 "#00FF00FF", "#00FF80FF", "#00FFFFFF", "#0080FFFF",
                 "#FF00FFFF", "#000000FF", "#0000FFFF")
+
+# To plot a barplot reporting winners ordered by their number of wins
 ggplot(data=top_winners_gt4, aes(x=WINNER, y=NUM_WINS, fill=NUM_WINS)) + geom_bar(stat='identity') + coord_flip() + guides(fill=FALSE) + scale_fill_gradientn(colours = the_colours)
 
+# To plot barplot by tournaments of cahpions having more than 4 grand slams
 Slam_Top_Chart_by_Trn = Slam_Winners %>% filter(WINNER %in% top_winners_gt4$WINNER) %>% group_by(TOURNAMENT, WINNER) %>% summarise(NUM_WINS=n()) %>% arrange(desc(NUM_WINS))
 Slam_Top_Chart_by_Trn$NUM_WINS <- factor(Slam_Top_Chart_by_Trn$NUM_WINS)
 
 ggplot(data=Slam_Top_Chart_by_Trn, aes(x=WINNER, y=NUM_WINS, fill=NUM_WINS)) + geom_bar(stat='identity') + coord_flip() + guides(fill=FALSE) + scale_y_discrete() + facet_grid(. ~ TOURNAMENT)
 
+# Heatmap: To highlight how many times champions met each other on Grand Slam tournament finals
 tl_rec <- 1:40
 winner_runnerup <- Slam_Winners[tl_rec, c("WINNER", "RUNNER.UP")]
 winner_runnerup_names <- unique(c(winner_runnerup[,1], winner_runnerup[,2]))
@@ -60,6 +70,7 @@ heatmap.2(match_matrix,
           dendrogram = "none",     # only draw a row dendrogram
           Colv = "NA")
 
+# Dendrogram plot: To group champions based on some specific similarity metric
 ch_n <- 1:20
 wins <- Slam_Top_Chart[ch_n, -1]
 D_Wins <- dist(wins, method = "euclidean")
@@ -69,6 +80,7 @@ groups <- cutree(hclust_fit, h = h_value)
 plot(hclust_fit, labels = Slam_Top_Chart$WINNER[ch_n], main = "Champions Dendrogram")
 rect.hclust(hclust_fit, h = h_value, border = "blue")
 
+# To plot Grand Slam Timeline 2013-2017: To highlight how the wins sequence happened 
 year_to_date_trnm <- function(the_year, the_trnm) {
   the_date <- NULL
   if (the_trnm == "Australian Open") {
@@ -89,6 +101,7 @@ timelineS(Slam_Winners[tl_rec, c("WINNER", "YEAR_DATE")], main = "Grand Slam Tim
           scale = "month", scale.format = "%Y", label.cex = 0.7, buffer.days = 120,
           labels = paste(Slam_Winners[tl_rec, "WINNER"], Slam_Winners[tl_rec, "TOURNAMENT"]))
 
+# Chord Diagram to highlight relationship between champions (more than 10 grand slams) and tournaments wins 
 top_winners_gt10 = Slam_Top_Chart %>% filter(NUM_WINS > 10)
 
 slam_win_cnt = inner_join(Slam_Winners, top_winners_gt10) %>% select(TOURNAMENT, WINNER) %>%
@@ -96,6 +109,7 @@ slam_win_cnt = inner_join(Slam_Winners, top_winners_gt10) %>% select(TOURNAMENT,
 
 chordDiagram(slam_win_cnt)
 
+# To highlight individual champions’ strengths and weaknesses in each specific Grand Slam tournament using radar plot
 champion_radar_plot <- function(df, champion_name) {
   slam_win_cnt_chp = df %>% filter(WINNER == champion_name)
   chp_num_wins <- slam_win_cnt_chp$NUM_WINS
@@ -108,7 +122,6 @@ champion_radar_plot <- function(df, champion_name) {
              centerzero = TRUE, pcol = rgb(0.2, 0.5, 0.5, 0.9) , pfcol = rgb(0.2, 0.5, 0.5, 0.3),
              plwd = 1, cglcol = "grey", cglty = 1, axislabcol = "blue",
              vlcex = 0.8, calcex = 0.7, title = champion_name)
-  
 }
 
 champion_radar_plot(slam_win_cnt, "Roger Federer")
